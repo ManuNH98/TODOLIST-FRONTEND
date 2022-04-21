344e56a3d2275726892c7ba4fe0d8032a307f069
Manuel Navarro <manuelnhc@gmail.com>
2022-04-21 16:10:06 +0100

    Primer Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular';
+  title = 'Proyecto de Prueba de Manuel Navarro';
+
+  tasks = [{
+    "userId": 1,
+    "id": 6,
+    "title":"Hola Jefe como estás",
+    "completed": true,  
+  },
+  {
+    "userId": 2,
+    "id": 5,
+    "title":"Hola Pepe como estás",
+    "completed": true,  
+  },
+  {
+    "userId": 3,
+    "id": 4,
+    "title":"Hola Juan como estás",
+    "completed": true,  
+  },
+  {
+    "userId": 4,
+    "id": 3,
+    "title":"Hola Ricardo como estás",
+    "completed": true,  
+  },
+  {
+    "userId": 5,
+    "id": 2,
+    "title":"Hola Pedro como estás",
+    "completed": true,  
+  }
+
+  ];
+
+  clickaronEn(title : string){
+    alert('Clickaron en aquella con titulo '+ title);
+  }
 }
